7c37a4bc8cd497a5dbff1ca482725f63b161dfbb
23tae <taehoonkim.dev@gmail.com>
2024-12-10 12:15:02 +0900

    fix: 외부이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-   remotePatterns: [
-     {
-       protocol: 'http',
-       hostname: 'localhost',
-       port: '8000',
-       pathname: '/static/**',
-     },
-   ],
- },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/static/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sinotale-backend-production.up.railway.app',
+        port: '',
+        pathname: '/static/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
